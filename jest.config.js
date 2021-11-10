7c709d5e85da81ff0eef0a0cfa8a70059350de4b
yasuo kurihara <breitling1216@gmail.com>
2021-11-10 22:48:05 +0900

    set detox

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verbose: true,
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', __dirname],
+  testRunner: 'jest-circus/runner',
+};
